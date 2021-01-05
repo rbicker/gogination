fe3055b77114f189aa8bc4cc4f9bdea5d0a3f409
Raphael Bicker <rbicker@nbarcrbi01.arcdom.local>
2021-01-05 19:57:16 +0100

    fixed typo in errors

--- errors.go
+++ errors.go
@@ -1,13 +1,14 @@
 package gogination
 
 type constError string
+
 func (err constError) Error() string {
 	return string(err)
 }
 
 // errors
 const (
-	ErrExceptedStruct  = constError("struct type object expected")
-	ErrNoIdField = constError("given object does not have an ID field")
+	ErrExpectedStruct = constError("struct type object expected")
+	ErrNoIdField      = constError("given object does not have an ID field")
 	ErrInvalidOrderBy = constError("given order by document is invalid")
 )
